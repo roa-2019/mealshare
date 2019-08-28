fc2ba126b57190bf011884194b1d210123088b24
chloe <chloeseunpark@gmail.com>
2019-08-28 20:52:12 +1200

    clean out default handlebar content

--- mealshare/server.js
+++ mealshare/server.js
@@ -6,10 +6,16 @@ const userRoutes = require('./routes/users')
 const server = express()
 
 // Middleware
-
-server.engine('hbs', hbs({extname: 'hbs'}))
+server.engine(
+  'hbs',
+  hbs({
+    defaultLayout: 'main',
+    extname: 'hbs'
+  })
+)
 server.set('view engine', 'hbs')
-server.use(express.urlencoded({extended: true}))
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
 
 // Routes
 
