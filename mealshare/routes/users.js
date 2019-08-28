6663e15522e7cd334d525b921184826b70ef8738
chloe <chloeseunpark@gmail.com>
2019-08-29 11:26:37 +1200

    add hardcoded html

--- mealshare/routes/users.js
+++ mealshare/routes/users.js
@@ -5,13 +5,7 @@ const db = require('../db')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('index', {users: users})
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  res.render('index')
 })
 
 module.exports = router
