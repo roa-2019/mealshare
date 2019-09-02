1590584fe01c9f61bcd1841b34289d9b7b9ca8ce
chloe <chloeseunpark@gmail.com>
2019-09-02 16:28:31 +1200

    add and delete ingredients && capitalise first letter of form data

--- mealshare/db.js
+++ mealshare/db.js
@@ -3,14 +3,32 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  getUser: getUser,
-  getUsers: getUsers
+  getUser,
+  getUsers,
+  addIngredient,
+  deleteIngredient
 }
 
-function getUsers (db = connection) {
-  return db('users').select()
+function getUsers(db = connection) {
+  return db('ingredients').select()
 }
 
-function getUser (id, db = connection) {
-  return db('users').where('id', id).first()
+function getUser(id, db = connection) {
+  return db('ingredients')
+    .where('id', id)
+    .first()
+}
+
+function addIngredient(formData, db = connection) {
+  return db('ingredients').insert({
+    quantity: formData.quantity,
+    ingredient: formData.ingredient,
+    name: formData.name
+  })
+}
+
+function deleteIngredient(id, db = connection) {
+  return db('ingredients')
+    .where('id', id)
+    .del()
 }
--- mealshare/routes/users.js
+++ mealshare/routes/users.js
@@ -10,7 +10,22 @@ router.get('/', (req, res) => {
 
 router.post('/add-ingredient', (req, res) => {
   let form = req.body
-  const {quantity, ingredient, name} = form
+  const formData = {
+    quantity: form.quantity,
+    ingredient: capitalizeFirstLetter(form.ingredient),
+    name: capitalizeFirstLetter(form.name)
+  }
+
+  db.addIngredient(formData).then(() => res.status(201).redirect('/'))
 })
+
+router.delete('/delete-ingredient/:id', (req, res) => {
+  let id = req.params.id
+  db.deleteIngredient(id)
+})
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
 module.exports = router
